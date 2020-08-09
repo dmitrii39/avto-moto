ea3a9188da00be4198ff2da6c682d1f8af4a08d3
dmitrii39 <d.tsyplov@mail.ru>
2020-08-09 23:54:56 +0200

    изменения 09.08.

--- js/main.js
+++ js/main.js
@@ -1,109 +1,111 @@
 "use strict";
 
-//  ---------------табы вкл выкл-------------
+//  ---------------табы вкл/выкл-------------
 (function () {
-    var button1 = document.querySelector(".car-about__button_1");
-    var button2 = document.querySelector(".car-about__button_2");
-    var button3 = document.querySelector(".car-about__button_3");
+  var button1 = document.querySelector(".car-about__button_1");
+  var button2 = document.querySelector(".car-about__button_2");
+  var button3 = document.querySelector(".car-about__button_3");
 
-    var button4 = document.querySelector(".button__init");
+  var button4 = document.querySelector(".button__init");
 
-    var carBlock1 = document.querySelector(".car-block1");
-    var carBlock2 = document.querySelector(".car-block2");
-    var carBlock3 = document.querySelector(".car-block3");
+  var carBlock1 = document.querySelector(".car-block1");
+  var carBlock2 = document.querySelector(".car-block2");
+  var carBlock3 = document.querySelector(".car-block3");
 
-    button1.addEventListener("click", function () {
-        carBlock1.classList.remove("hidden");
-        carBlock3.classList.add("hidden");
-        carBlock2.classList.add("hidden");
+  button1.addEventListener("click", function () {
+    carBlock1.classList.remove("hidden");
+    carBlock3.classList.add("hidden");
+    carBlock2.classList.add("hidden");
 
-        lowBlock.style.height = "900px";
-    });
+    lowBlock.style.height = "900px";
+  });
 
-    button2.addEventListener("click", function () {
-        carBlock2.classList.remove("hidden");
-        carBlock1.classList.add("hidden");
-        carBlock3.classList.add("hidden");
-        lowBlock.style.height = "900px";
-    });
+  button2.addEventListener("click", function () {
+    carBlock2.classList.remove("hidden");
+    carBlock1.classList.add("hidden");
+    carBlock3.classList.add("hidden");
+    lowBlock.style.height = "900px";
+  });
 
-    button3.addEventListener("click", function () {
-        carBlock3.classList.remove("hidden");
-        carBlock2.classList.add("hidden");
-        carBlock1.classList.add("hidden");
-        lowBlock.style.height = "650px";
-    });
+  button3.addEventListener("click", function () {
+    carBlock3.classList.remove("hidden");
+    carBlock2.classList.add("hidden");
+    carBlock1.classList.add("hidden");
+    lowBlock.style.height = "650px";
+  });
 
-    button4.addEventListener("click", function () {
-        popup.classList.remove("hidden");
-    });
+  var body = document.querySelector("body");
+  button4.addEventListener("click", function () {
+    popup.classList.remove("hidden");
+    body.style.overflow = "hidden";
+  });
 })();
 
+// ---------------убрать табы по нажатию--------
 
+(function () {
+  var carBlock = document.querySelectorAll(".car-block");
 
-
-var carBlock = document.querySelectorAll(".car-block");
-
-var removeClickListener = function (onCarBlockClick) {
+  var removeClickListener = function (onCarBlockClick) {
     onCarBlockClick.addEventListener("click", function () {
-        onCarBlockClick.classList.add("hidden");
+      onCarBlockClick.classList.add("hidden");
     });
-}
+  };
 
-for (var i = 0; i < carBlock.length; i++) {
+  for (var i = 0; i < carBlock.length; i++) {
     var onCarBlockClick = carBlock[i];
 
     removeClickListener(onCarBlockClick);
-}
-
+  }
+})();
 
 // ------------попап--------------------
 
 (function () {
-    var popup = document.querySelector(".w-modal");
-    var closeButton = document.querySelector(".close");
-    var lowBlock = document.querySelector(".low-block");
+  var popup = document.querySelector(".w-modal");
+  var closeButton = document.querySelector(".close");
+  var lowBlock = document.querySelector(".low-block");
 
-    closeButton.addEventListener("click", function () {
-        popup.classList.add("hidden");
-    });
+  closeButton.addEventListener("click", function () {
+    popup.classList.add("hidden");
+  });
 
-    document.addEventListener("keydown", function (evt) {
-        if (evt.key === "Escape") {
-            popup.classList.add("hidden");
-        }
-    });
+  document.addEventListener("keydown", function (evt) {
+    if (evt.key === "Escape") {
+      popup.classList.add("hidden");
+    }
+  });
 })();
 
 // -------------слайдер---------------
 (function () {
-    var bigPicture = document.querySelector(".photo__car__main");
-    var buttonPrew = document.querySelector(".button__prew");
-    var buttonNext = document.querySelector(".button__next");
-    var carouselItems = document.querySelectorAll(".carousel__item");
+  var bigPicture = document.querySelector(".photo__car__main");
+  var buttonPrew = document.querySelector(".button__prew");
+  var buttonNext = document.querySelector(".button__next");
+  var carouselItems = document.querySelectorAll(".carousel__item");
 
-    var activeSlide = 0;
+  var activeSlide = 0;
 
-    var onClickButtonNext = function () {
-        if (activeSlide < 2) {
-            activeSlide++;
-        }
-        bigPicture.querySelector("img").src = carouselItems[
-            activeSlide
-        ].querySelector("img").src;
-    };
+  var onClickButtonNext = function () {
+    if (activeSlide < 2) {
+      activeSlide++;
+    }
+    bigPicture.querySelector("img").src = carouselItems[
+      activeSlide
+    ].querySelector("img").src;
+  };
 
-    var onClickButtonPrew = function () {
-        if (activeSlide > 0) {
-            activeSlide--;
-        }
-        bigPicture.querySelector("img").src = carouselItems[
-            activeSlide
-        ].querySelector("img").src;
-    };
+  var onClickButtonPrew = function () {
+    if (activeSlide > 0) {
+      activeSlide--;
+    }
+    bigPicture.querySelector("img").src = carouselItems[
+      activeSlide
+    ].querySelector("img").src;
+  };
 
-    buttonNext.addEventListener("click", onClickButtonNext);
-    buttonPrew.addEventListener("click", onClickButtonPrew);
+  buttonNext.addEventListener("click", onClickButtonNext);
+  buttonPrew.addEventListener("click", onClickButtonPrew);
 })();
 
 // ------------Добавление отзывов из формы на страницу------------------------
@@ -131,44 +133,44 @@ var popupComments = popup.querySelector(".popup__comments");
 // ----------------------------
 
 (function () {
-    var sendler = buttonFeedback.addEventListener("click", function () {
-        carBlock2.appendChild(cloneElement);
-        popup.classList.remove("hidden");
-        templateButton.style.display = "none";
-        lowBlock.style.height = "1260px";
-        templateName.textContent = setupUserName.value;
-        templateAdvantages.textContent = popupAdvantages.value;
-        templatLimitations.textContent = popupLimitation.value;
-        templateComments.textContent = popupComments.value;
+  var sendler = buttonFeedback.addEventListener("click", function () {
+    carBlock2.appendChild(cloneElement);
+    popup.classList.remove("hidden");
+    templateButton.style.display = "none";
+    lowBlock.style.height = "1260px";
+    templateName.textContent = setupUserName.value;
+    templateAdvantages.textContent = popupAdvantages.value;
+    templatLimitations.textContent = popupLimitation.value;
+    templateComments.textContent = popupComments.value;
 
-        if (!setupUserName.value) {
-            sendler = false;
-            setupUserName.style.background = "red";
-            popupComments.style.background = "red";
-        }
-    });
+    if (!setupUserName.value) {
+      sendler = false;
+      setupUserName.style.background = "red";
+      popupComments.style.background = "red";
+    }
+  });
 })();
 
 // ------сохранение полей попапа в localstorage------------
 
 (function () {
-    setupUserName.value = localStorage.getItem("setupUserName");
-    setupUserName.addEventListener("input", function () {
-        localStorage.setItem("setupUserName", setupUserName.value);
-    });
+  setupUserName.value = localStorage.getItem("setupUserName");
+  setupUserName.addEventListener("input", function () {
+    localStorage.setItem("setupUserName", setupUserName.value);
+  });
 
-    popupAdvantages.value = localStorage.getItem("popupAdvantages");
-    popupAdvantages.addEventListener("input", function () {
-        localStorage.setItem("popupAdvantages", popupAdvantages.value);
-    });
+  popupAdvantages.value = localStorage.getItem("popupAdvantages");
+  popupAdvantages.addEventListener("input", function () {
+    localStorage.setItem("popupAdvantages", popupAdvantages.value);
+  });
 
-    popupLimitation.value = localStorage.getItem("popupLimitation");
-    popupLimitation.addEventListener("input", function () {
-        localStorage.setItem("popupLimitation", popupLimitation.value);
-    });
+  popupLimitation.value = localStorage.getItem("popupLimitation");
+  popupLimitation.addEventListener("input", function () {
+    localStorage.setItem("popupLimitation", popupLimitation.value);
+  });
 
-    popupComments.value = localStorage.getItem("popupComments");
-    popupComments.addEventListener("input", function () {
-        localStorage.setItem("popupComments", popupComments.value);
-    });
+  popupComments.value = localStorage.getItem("popupComments");
+  popupComments.addEventListener("input", function () {
+    localStorage.setItem("popupComments", popupComments.value);
+  });
 })();
